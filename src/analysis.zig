272a13cddd01cb00d9f59f52b19b68aa457e2dcc
Estelle Linkpy Reid <linkpydev@gmail.com>
2022-06-13 10:53:18 +0200

    Removed the reference to the ID Finder.

--- src/analysis.zig
+++ src/analysis.zig
@@ -1,4 +1,3 @@
 
-pub const IdentifierFinder = @import("./analysis/identifier_finder.zig");
 pub const IdentifierResolver = @import("./analysis/identifier_resolver.zig");
 pub const TypeResolver = @import("./analysis/type_resolver.zig");
